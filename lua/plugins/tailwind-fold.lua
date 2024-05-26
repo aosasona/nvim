bdbdd5bc8a61e2e7647121b3c8e20681a97083eb
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-05-26 23:42:18 +0100

    Added tailwind fold plugin

---
+++ lua/plugins/tailwind-fold.lua
@@ -0,0 +1,11 @@
+return {
+  "razak17/tailwind-fold.nvim",
+  opts = {},
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  event = "VeryLazy",
+  config = function()
+    require("tailwind-fold").setup {
+      ft = { "html", "twig", "blade", "php", "svelte", "vue", "astro", "typescriptreact", "gleam" },
+    }
+  end,
+}

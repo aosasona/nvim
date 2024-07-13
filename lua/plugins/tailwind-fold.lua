e894e5919f831eccef591bc4d6d54feadf5e397b
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-07-13 17:16:37 +0100

    Disable tailwind fold for Gleam

--- lua/plugins/tailwind-fold.lua
+++ lua/plugins/tailwind-fold.lua
@@ -5,7 +5,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("tailwind-fold").setup {
-      ft = { "html", "twig", "blade", "php", "svelte", "vue", "astro", "typescriptreact", "gleam" },
+      ft = { "html", "twig", "blade", "php", "svelte", "vue", "astro", "typescriptreact" },
     }
   end,
 }
